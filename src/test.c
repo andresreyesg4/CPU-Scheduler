1bb55403a678955af030a7c2e50f36c477cfda31
andresreyesg4 <71036855+andresreyesg4@users.noreply.github.com>
2021-02-14 15:08:00 -0500

    Add files via upload

---
+++ src/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+
+	char *command;
+	printf("size of char pointer %d\n", sizeof(command));
+	printf("size of char %d\n", sizeof(&command));
+	
+
+
+
+
+
+	return 1;
+
+}
